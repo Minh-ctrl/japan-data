15f487e525784b4712bd330e9d7891ad4c611544
minh-ctrl <binhminh140899@gmail.com>
2022-02-16 14:34:26 +0900

    feat: 🎸 moving to vue

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <button @click="toggleData">Show</button>
+
+    <br />
+    <input type="text" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  el: "#data",
+  data: function () {
+    return {
+      info: null,
+      isVisible: false,
+      message: "Hello Vue!",
+    };
+  },
+  methods: {
+    async toggleData() {
+      await console.log(this.info.ageData);
+    },
+  },
+  mounted: function () {
+    axios
+      .get("http://localhost:5000/fetch-population")
+      .then((res) => (this.info = res.data));
+  },
+};
+</script>
+
+<style scoped>
+</style>
