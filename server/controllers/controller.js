5b034e1faef1ecdc9d0e63a9fccec3f25fa1ceb3
minh-ctrl <binhminh140899@gmail.com>
2022-04-09 22:31:14 +0900

    feat: 🎸 data formatting a bit

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -12,10 +12,14 @@ exports.fetchAgeData = async (req, res) => {
   let ageData = [];
   for (const property in ageGrouped) {
     let newObj = {};
+    let res;
     for (let i = 0; i < ageGrouped[property].length; i++) {
-      newObj[ageGrouped[property][i].year] =
-        parseFloat(ageGrouped[property][i].avg_population);
+      newObj[ageGrouped[property][i].year] = parseFloat(
+        ageGrouped[property][i].avg_population
+      );
     }
+    let data = Object.entries(newObj);
+    console.log(newObj);
     obj = {
       description: {
         english_name: ageGrouped[property][0].english_name,
@@ -26,7 +30,7 @@ exports.fetchAgeData = async (req, res) => {
     ageData.push(obj);
   }
   let population = await models.population_number_prefectures.findAll();
-  let industry  = await models.industry_employment_population.findAll();
+  let industry = await models.industry_employment_population.findAll();
   let db = { ageData, population, industry };
   res.json(db);
 };
